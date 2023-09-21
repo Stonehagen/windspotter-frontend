cace168379c896e889f17f90dd338968a3994154
Stonehagen <steinhagen@mailbox.org>
2023-09-21 21:37:01 +0200

    add svgs to display cloud coverage

--- src/components/OneDayTable.jsx
+++ src/components/OneDayTable.jsx
@@ -1,14 +1,29 @@
 import moment from 'moment';
 import WindDir from '../assets/WindDir.svg?react';
+import ClearDay from '../assets/weather/ClearDay.svg?react';
+import PartlyCloudyDay from '../assets/weather/PartlyCloudyDay.svg?react';
+import MostlyCloudyDay from '../assets/weather/MostlyCloudyDay.svg?react';
+import Cloudy from '../assets/weather/Cloudy.svg?react';
 
 import { getColorGradeWind, getColorGradeTemp } from '../methods/getColorGrade';
 import getWindSpeed from '../methods/getWindSpeed';
-import getWindDirection from '../methods/getWindDirection';
 
 const OneDayTable = ({ dayArray, index }) => {
   const windUnit = 'kts';
   const getPrettyDate = (time) => {
     return moment(time).format('dddd, DD.MM.YY');
+  };
+
+  const getWeatherIcon = (cloudCover) => {
+    if (cloudCover <= 0.25) {
+      return <ClearDay />;
+    } else if (cloudCover <= 0.5) {
+      return <PartlyCloudyDay />;
+    } else if (cloudCover <= 0.75) {
+      return <MostlyCloudyDay />;
+    } else {
+      return <Cloudy />;
+    }
   };
 
   const createTableRow = (timeframe) => {
@@ -18,26 +33,25 @@ const OneDayTable = ({ dayArray, index }) => {
           <div>{moment(timeframe.time - 2 * 6000).format('HH')}</div>
         </td>
         <td
-          style={{
-            backgroundColor: getColorGradeWind(timeframe.ws),
-          }}
-        >
-          <WindDir style={{ transform: `rotate(${timeframe.dir + 180}deg)` }} />
-        </td>
-        <td
-          className="windSpeeds"
+          className="windForecast"
           style={{
             background: `linear-gradient(to right, ${getColorGradeWind(
               timeframe.ws,
-            )}, ${getColorGradeWind(timeframe.wsMax)})`,
+            )} 40%, ${getColorGradeWind(timeframe.wsMax)} 80%)`,
           }}
         >
-          <div className="wind">{`${getWindSpeed(
-            timeframe.ws,
-            windUnit,
-          )} ${windUnit}`}</div>
-          <div className="windGust">
-            max {`${getWindSpeed(timeframe.wsMax, windUnit)} ${windUnit}`}
+          <WindDir
+            className="windDirection"
+            style={{ transform: `rotate(${timeframe.dir + 180}deg)` }}
+          />
+          <div className="windSpeeds">
+            <div className="wind">{`${getWindSpeed(
+              timeframe.ws,
+              windUnit,
+            )} ${windUnit}`}</div>
+            <div className="windGust">
+              max {`${getWindSpeed(timeframe.wsMax, windUnit)} ${windUnit}`}
+            </div>
           </div>
         </td>
         <td
@@ -46,7 +60,9 @@ const OneDayTable = ({ dayArray, index }) => {
         >
           {timeframe.t.toFixed(0)}˚C
         </td>
-        <td>{timeframe.clouds.toFixed(2)}</td>
+        <td className="clouds">
+          {getWeatherIcon(timeframe.clouds)}
+        </td>
         <td>{timeframe.rain.toFixed(2)}</td>
       </tr>
     );
@@ -60,7 +76,6 @@ const OneDayTable = ({ dayArray, index }) => {
         </tr>
         <tr className="windHeading">
           <th>h</th>
-          <th>dir</th>
           <th>wind</th>
           <th>temp</th>
           <th>cloud</th>
