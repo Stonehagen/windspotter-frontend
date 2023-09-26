fdfd039fed70a20e2f354db7a3a866cb2c1e5994
Stonehagen <steinhagen@mailbox.org>
2023-09-26 13:49:16 +0200

    changes imports to work again

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import '../styles/App.css';
+import './styles/App.css';
 
 import Forecast from './components/Forecast';
 import LandingPage from './components/LandingPage';
