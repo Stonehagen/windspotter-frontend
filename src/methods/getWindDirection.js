d3369c62ab4c0eeb1d7552bcaa6648627cd6e62f
Stonehagen <steinhagen@mailbox.org>
2023-11-07 09:14:56 +0100

    change getWindDirections method to have access to the directions pattern

--- src/methods/getWindDirection.js
+++ src/methods/getWindDirection.js
@@ -1,23 +1,24 @@
+export const directions = [
+  'N',
+  'NNE',
+  'NE',
+  'ENE',
+  'E',
+  'ESE',
+  'SE',
+  'SSE',
+  'S',
+  'SSW',
+  'SW',
+  'WSW',
+  'W',
+  'WNW',
+  'NW',
+  'NNW',
+  'N',
+];
+
 const getWindDirection = (deg) => {
-  const directions = [
-    'N',
-    'NNE',
-    'NE',
-    'ENE',
-    'E',
-    'ESE',
-    'SE',
-    'SSE',
-    'S',
-    'SSW',
-    'SW',
-    'WSW',
-    'W',
-    'WNW',
-    'NW',
-    'NNW',
-    'N',
-  ];
   const index = Math.round(deg / 22.5);
   const direction = directions[index];
   return direction;
