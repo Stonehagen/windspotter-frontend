741e978c5a9ce045b5a73c947a60a0fbce807573
Stonehagen <steinhagen@mailbox.org>
2023-11-13 11:51:51 +0100

    add layer for windforecast

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,21 +3,47 @@ import {
   MapContainer,
   TileLayer,
   AttributionControl,
-  LayerGroup,
   LayersControl,
+  ImageOverlay,
+  LayerGroup,
 } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import WindLayer from './WindLayer';
-import jsonData from '../assets/wind-global.json';
 import L from 'leaflet';
+import '@maplibre/maplibre-gl-leaflet';
+import 'leaflet/dist/leaflet.css';
+import mapStyle from '../assets/maps/alidade_smooth-overlay.json'
+
+import image from '../assets/windImg.png';
 
 const Map = () => {
   const [map, setMap] = useState(null);
   const [layersControl, setLayersControl] = useState(null);
+  const [done, setDone] = useState(false);
 
   useEffect(() => {
-    if (layersControl && map) {
+    if (layersControl && map && !done) {
       map.removeControl(layersControl);
+
+      L.imageOverlay(
+        image,
+        [
+          [58.08, -3.94],
+          [43.18, 20.34],
+        ],
+        { zIndex: 0 },
+      ).addTo(map);
+
+      L.maplibreGL({
+        style: mapStyle,
+        attribution:
+          '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/about" target="_blank">OpenStreetMap</a> contributors',
+        pane: 'overlayPane',
+        zIndex: 1,
+      }).addTo(map);
+      setDone(true);
+    }
+    if (map) {
+      map.getPanes().mapPane.style.opacity = 0.7;
+      console.log(map.getPanes());
     }
   }, [map]);
 
@@ -32,28 +58,7 @@ const Map = () => {
         ref={setMap}
         zoomControl={false}
       >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <LayersControl ref={setLayersControl}>
-          <LayersControl.BaseLayer checked name="Wind">
-            <WindLayer
-              displayValues={false}
-              displayOptions={{
-                velocityType: 'GBR Wind',
-                displayPosition: 'bottomright',
-                displayEmptyString: 'No wind data',
-              }}
-              data={jsonData}
-              maxVlocity={15}
-              minVelocity={0}
-              opacity={0.97}
-              colorScale={['#ffffff']}
-            />
-          </LayersControl.BaseLayer>
-        </LayersControl>
-
+        <LayersControl ref={setLayersControl}></LayersControl>
         <AttributionControl position="bottomright" prefix="" />
       </MapContainer>
     </>
