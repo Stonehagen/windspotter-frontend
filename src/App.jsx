6f11db7223cd5182fdf48a35456dc88a53308eca
Stonehagen <steinhagen@mailbox.org>
2023-11-30 22:43:14 +0100

    change height on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './styles/App.css';
 import 'maplibre-gl';
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+import Div100vh from 'react-div-100vh';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Div100vh className='root'>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Div100vh>,
 );
