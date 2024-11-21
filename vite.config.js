bf6e20c42f28a4fa57274d26893fb2faf9540b92
Stonehagen <steinhagen@mailbox.org>
2024-11-21 16:11:07 +0100

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,35 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 
-const manifestForPlugin = {
-  registerType: "prompt",
-  manifest: {
-    name: "Windmate",
-    short_name: "Windmate",
-    start_url: "/",
-    display: "standalone",
-    background_color: "#ffffff",
-    theme_color: "#000000",
-    description: "An App to get you the best Windforecast.",
-    icons: [
-      {
-        src: "/windmate192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/windmate512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-  },
-  workbox: {
-    dontCacheBustURLsMatching: /.\.(?:png|jpg|jpeg|svg)$/,
-  },
-};
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), VitePWA(manifestForPlugin)],
+  plugins: [react(), svgr(), VitePWA({ registerType: 'autoUpdate' })],
 })
