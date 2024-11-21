2a1822f400b998e2ae8b799cccd73e9d0defd320
Stonehagen <steinhagen@mailbox.org>
2024-11-21 16:23:18 +0100

    remove workbox

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,42 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"Windmate",
+    short_name:"Windmate",
+    description:"I am a Windforecast App",
+    icons:[{
+      src: '/windmate192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'/windmate512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/windmate512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [react(), svgr(), VitePWA(manifestForPlugIn)],
 })
