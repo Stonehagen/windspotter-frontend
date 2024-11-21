e84cd745e68d1519839c7c3fe391cc92f32a9daa
Stonehagen <steinhagen@mailbox.org>
2024-11-21 15:54:35 +0100

    add manifest plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 
+const manifestForPlugin = {
+  registerType: "prompt",
+  manifest: {
+    name: "Windmate",
+    short_name: "Windmate",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    description: "An App to get you the best Windforecast.",
+    icons: [
+      {
+        src: "/windmate192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/windmate512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), VitePWA(manifestForPlugin)],
 })
